54646e62e92aee2774fd2b1106a2da15d1a67fdd
JoaquinLoaiza <joaquin.loaiza@ucsm.edu.pe>
2024-09-09 12:16:48 -0500

    Se añadio la actividad 1,2,3  dek trabajo_3

---
+++ Trabajo_4/Actividades/Actividad_1.java
@@ -0,0 +1,24 @@
+package Trabajo_4.Actividades;
+
+
+import java.util.ArrayList;
+
+public class Actividad_1 {
+}
+
+
+
+class Contacto {
+    String nombre;
+    String pais;
+    int numero;
+
+
+}
+
+
+class Gestor{
+    ArrayList<Contacto> contactos;
+
+
+}
